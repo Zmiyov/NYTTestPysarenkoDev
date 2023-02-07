a3b4db224466b3dea1edb868d97f59ee85a244d2
Volodymyr <zmiyov88@gmail.com>
2023-02-07 18:31:34 +0200

    Cleaned api manager

--- NYTTestPysarenkoDev/Services/Networking/NYTAPIManager.swift
+++ NYTTestPysarenkoDev/Services/Networking/NYTAPIManager.swift
@@ -14,8 +14,7 @@ final class NYTAPIManager {
     
     static let shared = NYTAPIManager()
     
-    var categories: [CategoryModel] = []
-    
+    //For codable models
     func fetchCategories(completion: @escaping ([CategoryModel]) -> Void) {
         let url = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=\(NYTAPIKey.key.rawValue)"
         AF.request(url).responseDecodable(of: ResponseCategories.self) { response in
@@ -34,22 +33,19 @@ final class NYTAPIManager {
         }
     }
     
+    //For data provider
     func fetchCategoriesJSON(completion: @escaping(_ categoriesDict: [[String: Any]]?, _ error: Error?) -> ()) {
         let url = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=\(NYTAPIKey.key.rawValue)"
         AF.request(url).responseData { data in
             switch data.result {
                 
             case .success(let data):
-                
                 let jsonObject = try? JSONSerialization.jsonObject(with: data, options: [])
                 let jsonDictionary = jsonObject as? [String: Any]
-//                let results = jsonDictionary?["results"] as? [String: Any]
                 let categories = jsonDictionary?["results"] as? [[String: Any]]
-
-                completion(categories, nil)
-                
 //                let json = try? JSON(data: data)
-//                print(books)
+//                print(json)
+                completion(categories, nil)
             case .failure(let error):
                 completion(nil, error)
                 print(error)
@@ -64,31 +60,15 @@ final class NYTAPIManager {
             switch data.result {
                 
             case .success(let data):
-                
                 let jsonObject = try? JSONSerialization.jsonObject(with: data, options: [])
                 let jsonDictionary = jsonObject as? [String: Any]
                 let results = jsonDictionary?["results"] as? [String: Any]
                 let books = results?["books"] as? [[String: Any]]
-
-                completion(books, nil)
-                
 //                let json = try? JSON(data: data)
-//                print(books)
+//                print(json)
+                completion(books, nil)
             case .failure(let error):
                 completion(nil, error)
-                print(error)
-            }
-        }
-    }
-
-    
-    func fetchImage(url: String, completion: @escaping (UIImage) -> Void) {
-        AF.request(url).responseImage { response in
-            switch response.result {
-                
-            case .success(let image):
-                completion(image)
-            case .failure(let error):
                 print(error)
             }
         }
