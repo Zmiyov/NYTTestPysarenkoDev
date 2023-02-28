7db50559a8250c9ace07f874c16b6b91d1db7fff
Volodymyr <zmiyov88@gmail.com>
2023-02-28 11:51:59 +0200

    Added codable fetch func

--- NYTTestPysarenkoDev/Services/Networking/NYTAPIManager.swift
+++ NYTTestPysarenkoDev/Services/Networking/NYTAPIManager.swift
@@ -13,6 +13,26 @@ import SwiftyJSON
 final class NYTAPIManager {
 
     static let shared = NYTAPIManager()
+    
+    //MARK: - For codable models
+    
+    func fetchCategories(completion: @escaping ([CategoryModel]) -> Void) {
+        let url = "https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=\(NYTAPIKey.key.rawValue)"
+        AF.request(url).responseDecodable(of: ResponseCategories.self) { response in
+            guard let fetchedCategories = response.value?.categories else { return }
+
+            completion(fetchedCategories)
+        }
+    }
+    
+    func fetchBooks(name: String, date: String, completion: @escaping ([BookModel]) -> Void) {
+        let url = "https://api.nytimes.com/svc/books/v3/lists/\(date)/\(name).json?api-key=\(NYTAPIKey.key.rawValue)"
+        AF.request(url).responseDecodable(of: ResponseBooks.self) { response in
+            guard let fetchedBooks = response.value?.books else { return }
+
+            completion(fetchedBooks)
+        }
+    }
 
     // MARK: - For data provider(are using)
 
